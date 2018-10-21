398b664c68335c153feb20280366a82ec00a67f1
Matthew Talbot <mdtalbot@gmail.com>
2018-10-21 18:43:47 -0400

    here goes nothing

---
+++ src/components/NewsContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const newsAPIKey = "96b12097d8aa4b1ea1bb29951713eb6f"
+
+export default class NewsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
